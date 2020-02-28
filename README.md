52d25d61b04142aee576cada22e856cc89defd83
Mike Domingues <mikeicd@gmail.com>
2020-02-27 23:56:02 -0300

    Alterado o codigo para ler o argumento e separar a palavra que vai ser substituida e a nova. Feito a organiação do formulário o codigo deve aceitar qualquer tipo de mudança...

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# createForm
-Tarefa 2, prog2, IFSC-SJ, Eng Telecom.
+#Manual
+O programa deve ser executado em sitemas operacionais Linux. Baixe o arquivo **creatForm** e utilizando o terminal inicie ele conforme o codigo a baixo (certifique-se que o formulário está na raiz onde o programa foi salvo):
+
+`./creatForm form.txt fruta="cebola" legume="tomate"`
+
+O formulário também segue um padrão onde palavras chaves que serão substituidas devem ser escritas com o simbolo $ na frente, ex: Estamos em falta de `$fruta` no estoque.
+
+**form.txt** - Troque pelo nome do arquivo que contem o modelo do formulário;
+
+**fruta** - Substitua pela palavra que será alterada no formulário;
+
+**cebola** - Substitua pela palavra final que deseja (a palavra deve estar entre aspas duplas).
+
+
+
+#Sobre o código
+O programa verfica os argumentos inseridos pelo usuário, e os sepera em dois vetores de strigs, o keyWord e o subWord.
+
+É realizado um find para achar a posição do **"="** e assim feito a separação do argumento para savar nos vetores keyWord e subWord.
+
+Após isso é feito a leitura do do formulário e usando o find e replace para achar as palavras no formulário e substituir keyWord por subWord.
+
+No final é feito o print na tela do formulário já alterado.
+
+#Resolução
+Creio ter atendido todos os requisitos propostos.
--- main.cpp
+++ main.cpp
@@ -4,10 +4,9 @@
 #include <stdio.h>
 #include <errno.h>
 
-
 using namespace std;
 
-string replaceThis(string str, const string& from, const string& to) {
+string replaceThis(string str, const string &from, const string &to) {
     size_t startPos = 0;
     while ((startPos = str.find(from, startPos)) != std::string::npos) {
         str.replace(startPos, from.length(), to);
@@ -17,24 +16,31 @@ string replaceThis(string str, const string& from, const string& to) {
 }
 
 int main(int argc, char *argv[]) {
+    string linha, argumentos[argc -2], keyWord[argc -2], subWord[argc -2];
 
-    std::string s(argv[1]);
+    size_t startPos=0;
+    for (int i = 0; i < argc -2; ++i) {
+        argumentos[i] = argv[i+2];
+        startPos = argumentos[i].find("=", startPos);
+        keyWord[i] = "$" + argumentos[i].substr(0, startPos);
+        subWord[i] = argumentos[i].substr(startPos+1, argumentos[i].length()-1);
+        startPos=0;
+    }
 
-    ifstream arq("../" + s);
+    std::string filePath(argv[1]);
+    ifstream arq(filePath);
 
     if (!arq.is_open()) {
         perror("Ao abrir arquivo");
         return errno;
     }
-    string linha;
-    while (getline(arq, linha)) {
-        linha =  replaceThis(linha, "$cidade", argv[2]);
-        linha =  replaceThis(linha, "$data", argv[3]);
-        linha =  replaceThis(linha, "$aluno", argv[4]);
-        linha =  replaceThis(linha, "$disciplina", argv[5]);
 
+    while (getline(arq, linha)) {
+
+        for (int i = 0; i < argc-2; ++i) {
+            linha = replaceThis(linha, keyWord[i], subWord[i]);
+        }
         cout << linha << endl;
     }
-
     return 0;
 }
